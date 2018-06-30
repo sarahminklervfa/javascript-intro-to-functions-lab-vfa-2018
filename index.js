c7e683853f797f6c12d0aea977a3f37cf23eb2f5
Sarah Minkler <sarah.minkler@gmail.com>
2018-06-30 03:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }
   if (string === "I love you, Grandma.") {
-    return "I love you, too"
+    return "I love you, too."
   }
 }
